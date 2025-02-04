2f19f6cd45a11f50a4dec6e6c31426e53953992b
Alex Maldonado <alex.maldonado@outlook.com>
2025-02-04 09:30:01 -0300

    Add Two Sum and Best Time to Buy/Sell Stock solutions
    - Implement Two Sum with HashMap approach (O(n) time)
    - Add Best Time to Buy/Sell Stock using one-pass solution

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.insert(0, '..')
+
+from algorithms.two_sum import two_sum
+from algorithms.best_time_to_buy_and_sell_stock import max_profit
+
+# print(two_sum([2,7,11,15], 9))
+# print(two_sum([3,2,4], 6))
+# print(two_sum([3,3], 6))
+# print(two_sum([4,2,5,0,6,3,2,7,1], 1))
+
+# print(max_profit([7,1,5,3,6,4]))
+# print(max_profit([7,6,4,3,1]))
